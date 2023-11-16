c931ffb599698f82c100d22e352a531b8a541adc
POKA TCHONENG Hermann <pokatchoneng@gmail.com>
2023-11-16 16:03:08 +0100

    Fixtures persisted on database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct(){
+        $this->faker = Factory::create("fr_FR");
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        for ($i=1; $i <=50 ; $i++) { 
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+                ->setPrice(mt_rand(0, 100));
 
+            $manager->persist($ingredient);
+        }
+       
+
+        
         $manager->flush();
     }
 }
--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -30,6 +30,10 @@ class Ingredient
     #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
 
+    public function __construct(){
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
