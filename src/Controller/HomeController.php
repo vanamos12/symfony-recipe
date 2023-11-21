fd82fc0cbeae1826ba8a043caed3240368e494ec
POKA TCHONENG Hermann <pokatchoneng@gmail.com>
2023-11-21 09:26:40 +0100

    On peut enregistrer un nouvel ingrédient dans la base de données.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController {
     #[Route('/', 'home.index', methods:['GET'])]
     public function index() : Response{
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
     }
 }
