bd6cf4f99a5fc45fea76bcc62c4bd8962633ddd3
POKA TCHONENG Hermann <pokatchoneng@gmail.com>
2024-01-31 17:32:24 +0100

    test the deletion of an ingredient

--- tests/Functional/IngredientTest.php
+++ tests/Functional/IngredientTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\Ingredient;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,7 +29,7 @@ class IngredientTest extends WebTestCase
 
         // Gérer le formulaire
         $form = $crawler->filter('form[name=ingredient]')->form([
-            'ingredient[name]' => "Un ingrédient 105",
+            'ingredient[name]' => "Un ingrédient 106",
             'ingredient[price]' => floatval(45),
         ]);
 
@@ -69,6 +70,69 @@ class IngredientTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Mes ingrédients');
 
         $this->assertSelectorTextContains('a.btn.btn-primary', 'Créer un ingrédient');
+
+        $this->assertRouteSame('ingredient.index');
+    }
+
+    public function testIfUpdateAnIngredientIsSuccessfull():void {
+        $client = static::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $user = $entityManager->find(User::class, 1);
+        $ingredient = $entityManager->getRepository(Ingredient::class)->findOneBy([
+            'user' => $user
+        ]);
+
+        $client->loginUser($user);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('ingredient.edit', ['id' => $ingredient->getId()])
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->filter('form[name=ingredient]')->form([
+            'ingredient[name]' => "Un ingrédient 107",
+            'ingredient[price]' => floatval(26),
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertSelectorTextContains('div.alert-success', 'Votre ingrédient a été modifiée avec succès !');
+
+        $this->assertRouteSame('ingredient.index');
+    }
+
+    public function testIfDeleteAnIngredientIsSuccessfull():void{
+        $client = static::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $user = $entityManager->find(User::class, 1);
+        $ingredient = $entityManager->getRepository(Ingredient::class)->findOneBy([
+            'user' => $user
+        ]);
+
+        $client->loginUser($user);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('ingredient.delete', ['id' => $ingredient->getId()])
+        );
+        
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertSelectorTextContains('div.alert-success', 'Un ingrédient a été supprimé avec succès !');
 
         $this->assertRouteSame('ingredient.index');
     }
